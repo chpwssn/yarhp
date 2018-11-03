d44d7e49588d01079ffd20def251095fe3d458cb
Chip Wasson <chip@wasson.io>
2018-11-02 18:34:58 -0600

    Add domain to post tile

--- src/components/RedditPost.tsx
+++ src/components/RedditPost.tsx
@@ -22,10 +22,10 @@ class RedditPost extends React.Component<props, state> {
       <div className="reddit-story story">
         <div className="link">
           <div className="title">
-            <span className="index">{this.props.index + 1}</span> <a href={post.url} target={this.props.target ? "_blank" : ""} >{post.title}</a>
+            <span className="index">{this.props.index + 1}</span> <a href={post.url} target={this.props.target ? "_blank" : ""} >{post.title}</a> <span className="domain">{post.subreddit_name_prefixed}</span>
           </div>
           <div className="details">
-            Posted {moment.unix(post.created).fromNow()} by {post.author} | <a href={`https://reddit.com/${post.permalink}`} target={this.props.target ? "_blank" : ""}>{post.num_comments} comments</a>
+            Posted {moment.unix(post.created).fromNow()} by {post.author} | <a href={`https://reddit.com${post.permalink}`} target={this.props.target ? "_blank" : ""}>{post.num_comments} comments</a>
           </div>
         </div>
       </div>
