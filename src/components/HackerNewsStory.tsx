8ede30ec8309d86d808d7962a2155442ff7ef1f8
Chip Wasson <chip@wasson.io>
2019-04-05 11:51:23 -0600

    Add ability to mute HN posts

--- src/components/HackerNewsStory.tsx
+++ src/components/HackerNewsStory.tsx
@@ -1,69 +1,110 @@
-import * as React from 'react';
-import hackernews, { Story } from '../lib/hackernews';
-import * as moment from 'moment';
+import * as React from "react";
+import hackernews, { Story } from "../lib/hackernews";
+import * as moment from "moment";
 
 interface props {
-  id: number,
-  index: number,
-  target: boolean,
+  id: number;
+  index: number;
+  target: boolean;
 }
 
 interface state {
-  story: Story,
-  domain: string | null
+  story: Story;
+  domain: string | null;
+  muted: boolean;
 }
 
-interface HackerNewsStory extends React.Component<props, state>{
-    domainRegExp: RegExp
+interface HackerNewsStory extends React.Component<props, state> {
+  domainRegExp: RegExp;
 }
 
 class HackerNewsStory extends React.Component<props, state> {
-
   constructor(props: props) {
     super(props);
-    this.domainRegExp = new RegExp(/^(?:https?:\/\/)?(?:www\.)?([^:\/\n?]+)/img)
+    this.domainRegExp = new RegExp(
+      /^(?:https?:\/\/)?(?:www\.)?([^:\/\n?]+)/gim
+    );
   }
 
   public componentDidMount = async () => {
-    this.setState({ story: await hackernews.getStory(this.props.id) }, () => {
-      this.setState({domain: this.domainFromLink(this.state.story.url)})
-    })
-  }
+    this.setState(
+      { story: await hackernews.getStory(this.props.id), muted: false },
+      () => {
+        this.setState({ domain: this.domainFromLink(this.state.story.url) });
+      }
+    );
+  };
+
+  public muteStory = () => {
+    const mutedStories = localStorage.getItem("hn_muted");
+    const muted =
+      mutedStories && mutedStories != ""
+        ? (JSON.parse(mutedStories) as number[])
+        : [];
+    muted.push(this.props.id);
+    localStorage.setItem("hn_muted", JSON.stringify(muted));
+    this.setState({ muted: true });
+  };
 
   private domainFromLink = (link: string): string | null => {
-      let match = this.domainRegExp.exec(link);
-      return match ? match[match.length - 1] : null;
-  }
+    let match = this.domainRegExp.exec(link);
+    return match ? match[match.length - 1] : null;
+  };
 
   private discussLink = (): string => {
     return `https://news.ycombinator.com/item?id=${this.state.story.id}`;
-  }
+  };
 
   private storyLink = (): string => {
     return this.state.story.url ? this.state.story.url : this.discussLink();
-  }
+  };
 
   private userLink = (): string => {
     return `https://news.ycombinator.com/user?id=${this.state.story.by}`;
-  }
+  };
 
   public render() {
     return (
       <div className="hn-story story">
         {this.state ? (
-          <div className="link">
-            <div className="title">
-              <span className="index">{this.props.index + 1}</span> <a href={this.storyLink()} target={this.props.target ? "_blank" : ""} >{this.state.story.title} {
-                this.state.domain ? (
-                  <span className="domain">({this.state.domain})</span>
-                ) : null
-              }</a>
+          this.state.muted ? (
+            <div>muted</div>
+          ) : (
+            <div className="link">
+              <div className="title">
+                <span className="index">{this.props.index + 1}</span>{" "}
+                <a
+                  href={this.storyLink()}
+                  target={this.props.target ? "_blank" : ""}
+                >
+                  {this.state.story.title}{" "}
+                  {this.state.domain ? (
+                    <span className="domain">({this.state.domain})</span>
+                  ) : null}
+                </a>
+              </div>
+              <div className="details">
+                {this.state.story.score} by{" "}
+                <a
+                  href={this.userLink()}
+                  target={this.props.target ? "_blank" : ""}
+                >
+                  {this.state.story.by}
+                </a>{" "}
+                {moment.unix(this.state.story.time).fromNow()} |{" "}
+                <a href={this.discussLink()} target="_blank">
+                  {this.state.story.descendants} comments
+                </a>{" "}
+                |{" "}
+                <span className="mute" onClick={this.muteStory}>
+                  mute
+                </span>
+              </div>
             </div>
-            <div className="details">
-              {this.state.story.score} by <a href={this.userLink()} target={this.props.target ? "_blank" : ""}>{this.state.story.by}</a> {moment.unix(this.state.story.time).fromNow()} | <a href={this.discussLink()} target="_blank">{this.state.story.descendants} comments</a>
-            </div>
-          </div>
-        ) : "Loading..."}
+          )
+        ) : (
+          "Loading..."
+        )}
       </div>
     );
   }
